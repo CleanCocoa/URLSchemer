f56ecfccee66d46ce730e0ec4bde5f3e466e89b9
Christian Tietze <me@christiantietze.de>
2023-11-28 08:59:56 +0100

    document Action

--- Sources/URLSchemer/Action.swift
+++ Sources/URLSchemer/Action.swift
@@ -1,7 +1,28 @@
+/// Parsed command from a URL.
+///
+/// In the context of a `module`, the `subject` is transformed via `verb` using either `object` or `payload` or  both (for verbs representing binary operators), or nothing (unary operators).
+///
+/// So for example `myapp://amplifier/volume/set/11` applies to the `amplifier` module, `set`ting (verb) its `volume` (subject) to `11` (object). This is a binary operator like `=` in programming. A verb like `reset` or `delete` would be a unary operator and work without an object.
 public struct Action {
     public let module: Module
     public let subject: String
     public let verb: String
+
     public let object: String?
+
     public let payload: Dictionary<String, String>?
+
+    public init(
+        module: Module,
+        subject: String,
+        verb: String,
+        object: String?,
+        payload: Dictionary<String, String>?
+    ) {
+        self.module = module
+        self.subject = subject
+        self.verb = verb
+        self.object = object
+        self.payload = payload
+    }
 }
