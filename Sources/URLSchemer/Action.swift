4de5b8f37a66748802bc387baab8079a0336bba1
Christian Tietze <me@christiantietze.de>
2023-11-29 12:04:39 +0100

    separate Action into BinaryAction and UnaryAction protocol

--- Sources/URLSchemer/Action.swift
+++ Sources/URLSchemer/Action.swift
@@ -1,9 +1,26 @@
+protocol UnaryAction {
+    associatedtype Subject
+    associatedtype Verb
+
+    var module: Module { get }
+    var subject: Subject { get }
+    var verb: Verb { get }
+    var payload: Payload? { get }
+}
+
+protocol BinaryAction: UnaryAction {
+    associatedtype Object
+    var object: Object { get }
+}
+
+public typealias Action = GenericAction
+
 /// Parsed command from a URL.
 ///
 /// In the context of a `module`, the `subject` is transformed via `verb` using either `object` or `payload` or  both (for verbs representing binary operators), or nothing (unary operators).
 ///
 /// So for example `myapp://amplifier/volume/set/11` applies to the `amplifier` module, `set`ting (verb) its `volume` (subject) to `11` (object). This is a binary operator like `=` in programming. A verb like `reset` or `delete` would be a unary operator and work without an object.
-public struct Action: Equatable {
+public struct GenericAction: Equatable, BinaryAction {
     public let module: Module
     public let subject: String
     public let verb: String
