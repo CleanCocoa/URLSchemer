c40ec0cdd817a3eaeab7521421cdd6c014b2cd5f
Christian Tietze <me@christiantietze.de>
2023-11-29 13:08:34 +0100

    make DeleteDefaults conform to SubjectVerbAction

--- Sources/URLSchemer/Preference/DeleteDefaults.swift
+++ Sources/URLSchemer/Preference/DeleteDefaults.swift
@@ -1,7 +1,14 @@
-public struct DeleteDefaults {
+public struct DeleteDefaults: Equatable {
     public let key: String
 
     public init(key: String) {
         self.key = key
     }
+}
+
+extension DeleteDefaults: SubjectVerbAction {
+    public var module: Module { .preference }
+    public var subject: String { key }
+    public var verb: String { "delete" }
+    public var payload: Payload? { nil }
 }
