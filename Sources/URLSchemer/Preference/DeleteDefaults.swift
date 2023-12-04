96a0e9f13e9075b10a07b394ea9f17728d6fd472
Christian Tietze <me@christiantietze.de>
2023-12-04 12:35:34 +0100

    specify Void instead of () as return type

--- Sources/URLSchemer/Preference/DeleteDefaults.swift
+++ Sources/URLSchemer/Preference/DeleteDefaults.swift
@@ -6,10 +6,10 @@ public struct DeleteDefaults: Equatable {
     }
 }
 
-extension DeleteDefaults: Action {   
+extension DeleteDefaults: Action {
     public var module: Module { .preference }
     public var subject: String { key }
     public var verb: String { "delete" }
-    public var object: () { () }
+    public var object: Void { () }
     public var payload: Payload? { nil }
 }
