09aa6f14c47a29c91baae0fbe4b67320a61a91cb
Christian Tietze <me@christiantietze.de>
2023-12-11 14:58:29 +0100

    add Payload.lowercased()

--- Sources/URLSchemer/Action/StringAction.swift
+++ Sources/URLSchemer/Action/StringAction.swift
@@ -38,8 +38,16 @@ extension StringAction {
             subject: subject.lowercased(),
             verb: verb.lowercased(),
             object: includingObject ? object?.lowercased() : object,
-            payload: payload?.map { ($0.lowercased(), $1?.lowercased()) }
+            payload: payload?.lowercased()
         )
+    }
+}
+
+extension Payload {
+    /// Transforms the dictionary into a new dictionary with both keys and values lowercased.
+    @inlinable
+    public func lowercased() -> Self {
+        map { ($0.lowercased(), $1?.lowercased()) }
     }
 }
 
